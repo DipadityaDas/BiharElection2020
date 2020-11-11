3f3ae9bc9741620b380cd7d08e56a33158908fea
Dipaditya Das <DipadityaDas@gmail.com>
2020-11-11 10:19:41 +0530

    Added url

--- Election.py
+++ Election.py
@@ -2,3 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
+
+# Official Website of Election Commission of India 
+url = "https://results.eci.gov.in/ACTRENDS2020/partywiseresult-S04.htm"
